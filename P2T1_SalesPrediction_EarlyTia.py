c34996c983124c73f195ddca2e516aed3aaf6d24
EarlyT <63275512+EarlyT@users.noreply.github.com>
2020-04-06 23:35:30 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_EarlyTia.py
@@ -0,0 +1,15 @@
+# A brief description of the project 
+# Date and class and name
+print('04/06/2020')
+print('CTI-110-P2T1 - Sales Prediction')
+print('Tia Early')
+#
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales:  '))
+
+# Calculatethe profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f')) 
